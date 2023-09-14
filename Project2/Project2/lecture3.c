3eac11490f6f47809398d7059d7c70e0257fd66b
codnjsj <codnjsj8627@gmail.com>
2023-09-14 14:55:57 +0900

    lecture1

---
+++ Project2/Project2/lecture3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(viod)
+{
+	int val_1, val_2, a, b;
+
+	printf("a=10, b=15¿œ∂ß \n");
+
+	a = 10;
+	b = 15;
+	val_1 = ++a + b--;
+
+	printf("val_1=%d, a=%d, b=%d", val_1, a, b);
+	
+	return 0;
+}
